116f94437670d07f785bb8c1022aee3f248cfa6a
Kauê Souza <161787698+kaue-souza7@users.noreply.github.com>
2026-03-02 21:45:08 -0300

    [feat] registrar aluno e professor

--- app/models.py
+++ app/models.py
@@ -20,4 +20,37 @@ class User(db.Model, UserMixin):
     tipo_usuario = db.Column(db.Enum(TipoUsuario), nullable=False)
 
 
+    professor = db.relationship("Professor", backref="user", uselist=False)
+
+
+
+class Aluno(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String, nullable=True)
+    sobrenome = db.Column(db.String, nullable=True)
+    cpf = db.Column(db.String(14), unique=True, nullable=False)
+    endereco = db.Column(db.String(255), nullable=False)
+    nascimento = db.Column(db.Date, nullable=False)
+    status = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+
+class Professor(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    cpf = db.Column(db.String(14), unique=True, nullable=False)
+    telefone = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    endereco = db.Column(db.String(255), nullable=False)
+    formacao = db.Column(db.String(150), nullable=False)
+    turno = db.Column(db.String(20), nullable=False)  # manhã, tarde, noite
+    nascimento = db.Column(db.Date, nullable=False)
+    status = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+
+
 
