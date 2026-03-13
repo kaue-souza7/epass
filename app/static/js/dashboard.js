7475c1de2c29754b392d87cad5a2a13ec09b5f77
Ana Caroline Moraes <166078471+Anymoraes5@users.noreply.github.com>
2026-03-12 23:55:23 -0300

    FEAT(dashboard) :  diferenciação dos cards especiais e adaptando os roots ao figma

---
+++ app/static/js/dashboard.js
@@ -0,0 +1,5 @@
+document.body.addEventListener("htmx:afterSwap", function(evt) {
+    if (evt.detail.target.id === "container-registro") {
+        evt.detail.target.style.display = "block";
+    }
+});
